bf7a2c6f7ceb8cc514de979cbf7f7bdf3bc3e4b1
dbarenas <dbarenas@hooptap.com>
2012-03-20 19:07:54 +0100

    mod2 omexpo.php css234

--- omexpo.php
+++ omexpo.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Reto Createsse Hooptap</title>
+<title>Reto Hooptap</title>
 </head>
 
 <body style="margin:0; padding:0; width:500;">
