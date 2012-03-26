b8eca5e64f6551e0857c07a49f3807ea30e36a55
dbarenas <dbarenas@hooptap.com>
2012-03-26 19:22:05 +0200

    add ranking.php mods

--- ranking.php
+++ ranking.php
@@ -10,18 +10,18 @@
 .avatar-wrap{width:125px;height:125px;border-radius:125px;position:relative;border:8px solid #0489B1;margin:0 0 3px 0}
  
 .box{border-radius:75px;position:relative;z-index:1; background-image:url(http://cdn.css-tricks.com/wp-content/themes/CSS-Tricks-9/images/brushed_alu.png); background-repeat:repeat;}
-.box-wrap{width:500px;height:800px;border-radius:75px;position:relative;border:8px solid #0489B1;margin:0 0 3px 0}
+.box-wrap{width:480px;height:800px;border-radius:75px;position:relative;border:8px solid #0489B1;margin:0 0 3px 0}
   
   </style>
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Reto Hooptap</title>
 </head>
-<body style="margin:0; padding:0; width:500;" >
-<div style="margin:0; padding:0; width:500px;" align="center" class="box box-500 box-wrap">
+<body>
+<div style="margin:0; padding:0; width:480px;" align="center" class="box box-500 box-wrap">
  
  <!-- 
-<div style="margin:0; padding:0; width:500px; height:320px; background:#ffffff 	url(http://176.34.252.128/preview/fb/hooptap_foto/images/FACEBOOK.jpg) no-repeat left top; font-family:Arial, Helvetica, sans-serif; font-size:12px; color:#FFFFFF;">
+<div style="margin:0; padding:0; width:480px; height:320px; background:#ffffff 	url(http://176.34.252.128/preview/fb/hooptap_foto/images/FACEBOOK.jpg) no-repeat left top; font-family:Arial, Helvetica, sans-serif; font-size:12px; color:#FFFFFF;">
        <div style="clear:both;"></div>
 </div> -->
   <a style="font-family: Happy Monkey, cursive; font-size:45px; color:#0489B1"> Ranking Reto </a>  <a style="font-family: Yellowtail, cursive; font-size:50px; color:#0489B1;">Buzz</a>
@@ -42,17 +42,17 @@ foreach ($con as $val) {
       $aux+=1;
       if($val->url != "")
       {
-    echo '<table width="500" border="0" cellspacing="0" cellpadding="0">  '
-    .'<tr><td width="15">'
+    echo '<table width="480" border="0" cellspacing="0" cellpadding="0">  '
+    .'<tr><td width="10">'
     .'<a style="font-family: Chango, cursive; font-size:45px; color:#0489B1">'.$aux.'.  </a>'
     .'<td> '
     .' <td width="125"><div  class="avatar-wrap"><img  class="avatar avatar-125 photo" src="http://prod.hooptap.com/media/userprofiles/' 
     . $val->url .'.jpg" width="125" height="125"/>'
     .'</div><td >'
-    . ' <td width="175">'
+    . ' <td width="165">'
     .'<a style="font-family: Happy Monkey, cursive; font-size:20px; color:#0489B1"> '.$val->nombre.' </a>'
 	.'</td> ' 
-	. ' <td width="85">'
+	. ' <td width="75">'
     .'<a style="font-family: Chango, cursive; font-size:20px; color:#0489B1">'.$val->mark.'</a><a style="font-family: Happy Monkey, cursive; font-size:20px; color:#0489B1;"> puntos </a>'
 		.'</td> '
 
