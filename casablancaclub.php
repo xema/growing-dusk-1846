442201edb4fb16e97622a28038401c44ee564d6a
dbarenas <dbarenas@hooptap.com>
2012-04-03 18:19:36 +0200

    Add covecas files

--- casablancaclub.php
+++ casablancaclub.php
@@ -5,82 +5,72 @@
 	<link href='http://fonts.googleapis.com/css?family=Happy+Monkey' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Yellowtail' rel='stylesheet' type='text/css'>
 	<style type="text/css">
-		.avatar{border-radius:100px;position:relative;z-index:1}
-		.avatar-wrap{width:100px;height:100px;border-radius:100px;position:relative;border:8px solid #0489B1;margin:0 0 3px 0}
-		 
-		.box{border-radius:75px;position:relative;z-index:1; background-color:#01A9DB;}
- 		.box-wrap{width:750px; border-radius:75px;position:relative;border:8px solid #0489B1;margin:0 0 3px 0}
-	</style>
-	<!-- 		.box{border-radius:75px;position:relative;z-index:1; background-image:url(./images/ios-linen.jpg); background-repeat:repeat;}
+		.avatar{border:100px;position:relative;z-index:1;background-color:#FFFFFF;}
+		.avatar-wrap{width:100px;height:100px;border:100px;position:relative;border:3px solid #FFFFFF;margin:0 0 3px 0}
+		 .box{border:5px;position:relative;z-index:1; background-color:#e8ebec;}
+		 .box-wrap{width:765px; height:965px;  border:5px;position:relative;border:0px solid #FFFFFF;margin:0 0 3px 0}
+	
+			</style>
+	<!-- 	
+ 		
+		.box{border-radius:75px;position:relative;z-index:1; background-image:url(./images/ios-linen.jpg); background-repeat:repeat;}
 	 -->
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Reto Hooptap</title>
 </head>
 <body>
-<div style="margin:0; padding:0; width:750px;" align="center" class="box box-750 box-wrap">
-  
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+ <div class="box box-780 box-wrap"  >
  
-<br> 
-  <a style="font-family: Happy Monkey; font-size:35px; color:#FBF8EF"> Ranking Reto Buzz</a>
-  <br>
-<a style="font-family: Happy Monkey; font-size:30px; color:#FBF8EF" >Hooptap</img></a>
-
-<ul style=" padding-left: 5px;">
+ <img src="http://prod.hooptap.com/preview/fb/ranking/heroku_file/images/cabecera_casa_blanca.jpg"  width="765px"></img>
+   
+ <div style="width: 100px; float: left; margin-left: 20px;" align="center" >
+<ul style=" padding-left: 2px;">
 <?php
-
 $contents = file_get_contents('http://prod.hooptap.com/preview/fb/ranking/list_gamer.php?rid=216');
+
+//$contents = file_get_contents('http://prod.hooptap.com/preview/fb/omexpo/listnofb.php');
 $con=json_decode($contents);
  
 $c = count($con);
 
-$s = ($c / 2); // change 3 to the number of columns you want to have.
+$s = ($c / 6); // change 3 to the number of columns you want to have.
 $aux=0;
 foreach ($con as $val) {
-
       $aux+=1;
-      if($val->url != "")
-      {
-    echo '<table width="175" border="0" cellspacing="0" cellpadding="0">  '
-		    .'<tr>'
-		    .' <td width="100"><div  class="avatar-wrap"><img  class="avatar avatar-100 photo" src="http://prod.hooptap.com/media/userprofiles/' 
-		    . $val->url .'.jpg" width="100" height="100"/>'
-		    .'</div><td >'
-		    . ' <td width="75">'
-		    .'<a style="font-family: Happy Monkey; font-size:20px; color:#FBF8EF"> '.$val->nombre.' </a>'
-			.'</td> ' 
-		    .'</tr>'
-    	.'</table>';
-      }
-      else
-      {
-    echo '<table width="175" border="0" cellspacing="0" cellpadding="0">  '
-		    .'<tr>'
-		    .' <td width="100"><div class="avatar-wrap" ><img   class="avatar avatar-100 photo" src="http://prod.hooptap.com/media/userprofiles/default.jpg" width="100" height="100"/>'
-		    .'</div><td >'
-		    . ' <td width="75">'
-		    .'<a style="font-family: Happy Monkey; font-size:20px; color:#FBF8EF;"> '.$val->nombre.' </a>'
-			.'</td> ' 
-		    .'</tr>'
-    	.'</table>';
-      }
-    if($i != 0 && $i % $s == 0)
+      $name=explode(" ",$val->nombre);
+    echo '<table width="100" border="0" cellspacing="0" cellpadding="0"> <tr><td"><div  class="avatar-wrap">'
+    .'<img  class="avatar avatar-100 photo" src="http://prod.hooptap.com/media/userprofiles/' 
+		   . $val->url .'.jpg" width="100" height="100"/>'
+		       .'</div></td></tr>'
+    	.'<tr align="center"><td ><p style="color: #414141; font-size: 10px; font-family: Arial, Helvetica, sans-serif; margin: 0;">'
+    	.' <span style="overflow:hidden; color: #000000; font-weight:bold; text-transform:uppercase; display: block; height: 15px; width: 100px;">'
+    	.$name[0]
+	.'</span></p>'
+	.'</td></tr></table>';
+    if($i != 0 && $i % $s == 0) 
     {
-?>
+        ?>
         </ul>
         </div>
-       <div style="width: 150px; float: left; margin-left: 20px;">
+       <div style="width: 100px; float: left; margin-left: 20px;">
         <ul style=" padding-left: 5px;">
         <?php
     }
     $i++;
-
 }
 ?>
-
 </ul>
-
-<div style="clear:both;"> </div>
- 
 </div >
+<div class="fb-comments" data-href="http://prod.hooptap.com/preview/fb/ranking/heroku_file/gintapas.php" data-num-posts="2" data-width="765"></div>
+  </div>
+
 </body>
 </html>
--- novecento.php
+++ novecento.php
@@ -5,95 +5,91 @@
 	<link href='http://fonts.googleapis.com/css?family=Happy+Monkey' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Yellowtail' rel='stylesheet' type='text/css'>
 	<style type="text/css">
-		.avatar{border-radius:100px;position:relative;z-index:1}
-		.avatar-wrap{width:100px;height:100px;border-radius:100px;position:relative;border:8px solid #0489B1;margin:0 0 3px 0}
-		 
-		.box{border-radius:75px;position:relative;z-index:1; background-color:#01A9DB;}
- 		.box-wrap{width:480px;border-radius:75px;position:relative;border:8px solid #0489B1;margin:0 0 3px 0}
-	</style>
-	<!-- 		.box{border-radius:75px;position:relative;z-index:1; background-image:url(./images/ios-linen.jpg); background-repeat:repeat;}
+		.avatar{border:100px;position:relative;z-index:1;background-color:#FFFFFF;}
+		.avatar-wrap{width:100px;height:100px;border:100px;position:relative;border:3px solid #FFFFFF;margin:0 0 3px 0}
+		 .box{border:5px;position:relative;z-index:1; background-color:#e8ebec;}
+		 .box-wrap{width:750px; height:600px;  border:5px;position:relative;border:0px solid #FFFFFF;margin:0 0 3px 0}
+	 a {  font-family:Arial;}
+			</style>
+	<!-- 	
+ 		
+		.box{border-radius:75px;position:relative;z-index:1; background-image:url(./images/ios-linen.jpg); background-repeat:repeat;}
 	 -->
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Reto Hooptap</title>
 </head>
 <body>
-<div style="margin:0; padding:0; width:480px;" align="center" class="box box-480 box-wrap">
-  
- 
-<br> 
-  <a style="font-family: Happy Monkey; font-size:35px; color:#FBF8EF"> Ranking Reto Buzz</a>
-  <br>
-<a style="font-family: Happy Monkey; font-size:30px; color:#FBF8EF" >Hooptap</img></a>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
-<ul style=" padding-left: 5px;">
+ 
+ <img src="http://prod.hooptap.com/preview/fb/ranking/heroku_file/images/cabecera_novecento.jpg"  width="765px"></img>
+ 
+
+<div>    
+<ul style=" padding-left: 5px; width:750px;">
+
 <?php
 
 $contents = file_get_contents('http://prod.hooptap.com/preview/fb/ranking/list_fast.php?rid=248');
-$con=json_decode($contents);
- 
+$con=json_decode($contents); 
 $c = count($con);
 
 $s = ($c / 1); // change 3 to the number of columns you want to have.
 $aux=0;
 foreach ($con as $val) {
-
-      $aux+=1;
+       $aux+=1;
+       $bgcolor = ($aux % 2 === 0) ? '#e8ebec' : '#FCFCFE';
+       
       if($val->url != "")
       {
-    echo '<table width="480" border="0" cellspacing="0" cellpadding="0">  '
-		    .'<tr><td width="15" align="right">'
-		    .'<a style="font-family: Chango, cursive; font-size:35px; color:#FBF8EF">'.$aux.'.</a>'
-		    .'<td> '
-		    .' <td width="100"><div  class="avatar-wrap"><img  class="avatar avatar-100 photo" src="http://prod.hooptap.com/media/userprofiles/' 
+    echo '<table align="center" width="750px" border="0" cellspacing="0" cellpadding="0" style="background-color:'.$bgcolor.' ">  '
+		    .'<tr><td width="200px" align="center">'
+		    .'<a style=" font-size:35px; color:#000000">'.$aux.'.</a>'
+		    .'</td> '
+		    .' <td width="200px"><div  class="avatar-wrap"><img  class="avatar avatar-100 photo" src="http://prod.hooptap.com/media/userprofiles/' 
 		    . $val->url .'.jpg" width="100" height="100"/>'
-		    .'</div><td >'
-		    . ' <td width="175">'
-		    .'<a style="font-family: Happy Monkey; font-size:20px; color:#FBF8EF"> '.$val->nombre.' </a>'
+		    .'</div></td >'
+		    . ' <td width="200px">'
+		    .'<a style=" font-size:20px; color:#000000"> '.$val->nombre.' </a>'
 			.'</td> ' 
-			. ' <td width="85">'
-		    .'<a style="font-family: Chango, cursive; font-size:20px; color:#FBF8EF">'
+			. ' <td width="150px">'
+		    .'<a style="  font-size:20px; color:#000000">&nbsp;&nbsp;&nbsp;'
 		    .$val->mark
-		    .'</a><br><a style="font-family: Happy Monkey; font-size:14px; color:#FBF8EF;"> segundos </a>'
+		    .'</a><br><a style="  font-size:14px; color:#000000;"> segundos </a>'
 			.'</td> '
 		    .'</tr>'
     	.'</table>';
       }
       else
       {
-    echo '<table width="480" border="0" cellspacing="0" cellpadding="0">  '
-		    .'<tr><td width="15" align="right">'
-		    .'<a style="font-family: Chango, cursive; font-size:35px; color:#FBF8EF;">'.$aux.'.</a>'
-		    .'<td> '
-		    .' <td width="100"><div class="avatar-wrap" ><img   class="avatar avatar-100 photo" src="http://prod.hooptap.com/media/userprofiles/default.jpg" width="100" height="100"/>'
-		    .'</div><td >'
-		    . ' <td width="175">'
-		    .'<a style="font-family: Happy Monkey; font-size:20px; color:#FBF8EF;"> '.$val->nombre.' </a>'
+    echo '<table align="center" width="750" border="0" cellspacing="0" cellpadding="0" style="background-color:'.$bgcolor.'>'
+		    .'<tr><td width="200px" align="center">'
+		    .'<a style="  font-size:35px; color:#000000;">'.$aux.'.</a>'
+		    .'</td> '
+		    .' <td width="200px"><div class="avatar-wrap" ><img   class="avatar avatar-100 photo" src="http://prod.hooptap.com/media/userprofiles/default.jpg" width="100" height="100"/>'
+		    .'</div></td >'
+		    . ' <td width="200px">'
+		    .'<a style="  font-size:20px; color:#000000;"> '.$val->nombre.' </a>'
 			.'</td> ' 
-			. ' <td width="85">'
-		    .'<a style="font-family: Chango, cursive; font-size:20px; color:#FBF8EF;">'
-		    .$val->mark.'</a><br><a style="font-family: Happy Monkey; font-size:14px; color:#FBF8EF;"> segundos </a>'
+			. ' <td width="150px">'
+		    .'<a style="  font-size:20px; color:#000000;">&nbsp;&nbsp;&nbsp;'
+		    .$val->mark.'</a><br><a style=" font-size:14px; color:#000000;"> segundos </a>'
 			.'</td> '
 		    .'</tr>'
     	.'</table>';
       }
-    if($i != 0 && $i % $s == 0)
-    {
-?>
-        </ul>
-        </div>
-       <div style="width: 150px; float: left; margin-left: 20px;">
-        <ul style=" padding-left: 5px;">
-        <?php
-    }
-    $i++;
-
 }
 ?>
 
+</table>
 </ul>
-
-<div style="clear:both;"><br></br></div>
- 
 </div >
 </body>
 </html>
