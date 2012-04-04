873e1e2ae4e579c1923e6be116df530e0b66aa6b
dbarenas <dbarenas@hooptap.com>
2012-04-04 17:11:33 +0200

    Add covecas files

--- list_fast.php
+++ list_fast.php
@@ -5,8 +5,13 @@ $con = conectame();
 $rid=$_GET['rid'];
  
 $sql=mysql_query("SELECT r.type, r.reto_id, r.user_id, r.mark AS mark, u.name AS name, u.imgid  AS url  FROM ranking_games AS r 
-LEFT JOIN users AS u ON u.id = r.user_id WHERE u.group_id =4 AND r.reto_id=$rid
-ORDER BY `r`.`mark` DESC LIMIT 0 , 5");
+LEFT JOIN users AS u ON u.id = r.user_id WHERE  r.type='parejas-abierto' AND r.reto_id=$rid AND u.group_id=4 
+ORDER BY `r`.`mark` ASC LIMIT 0 , 5 ");
+
+/*$sql=mysql_query("SELECT r.type, r.reto_id, r.user_id, r.mark AS mark, u.name AS name, u.imgid  AS url  FROM ranking_games AS r 
+LEFT JOIN users AS u ON u.id = r.user_id WHERE  r.type='parejas-abierto' AND r.reto_id=$rid   
+ORDER BY `r`.`mark` ASC LIMIT 0 , 5 ");
+ */
 
 while($row = mysql_fetch_array($sql))
 {
