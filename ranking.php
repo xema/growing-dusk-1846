dccf26e031e06ec7569819f4c5e846ebc373bcbe
Xema <xemaperez@gmail.com>
2012-05-28 11:49:41 +0200

    ranking buzz

--- ranking.php
+++ ranking.php
@@ -28,7 +28,7 @@
 <ul style=" padding-left: 5px;">
 <?php
 
-$contents = file_get_contents('http://prod.hooptap.com/preview/fb/ranking_buzz/listnofb.php');
+$contents = file_get_contents('http://prod.hooptap.com/preview/fb/ranking/listnofb.php');
 $con=json_decode($contents);
  
 $c = count($con);
