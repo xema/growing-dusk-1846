098cbf9506c58f84d3d92cf40c499b88f2d4a68a
Xema <xemaperez@gmail.com>
2012-06-01 16:04:17 +0200

    rankingII

---
+++ rankingII.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<style type="text/css">
+	.avatar{border:100px;position:relative;z-index:1;background-color:#FFFFFF; }
+	.avatar-wrap{width:100px;height:100px;border:100px;position:relative;border:3px solid #FFFFFF;float: left;}
+	.box{border:5px;position:relative;z-index:1; background-color:#e8ebec;}
+	.box-wrap{width:765px;   border:5px;position:relative;border:0px solid #FFFFFF;margin:0 0 3px 0}
+	.descp{font-family: 'Happy Monkey', cursive; font-size: 14px; font-weight: bold; width: 640px; float: left; color: #666; margin-right: 6px;}
+	.descp a{ color: #000;}
+	UL LI{ width: 734px; float: left; margin-bottom: 5px; margin-left: 16px;}
+	h3{ float: left; width:115px; height: 106px; text-align: center; margin: 0;}
+	a {  font-family:Arial;}	
+</style>
+<title>Documento sin t&iacute;tulo</title>
+</head>
+
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+ <div>
+ 	<div id="cabecera"><img src="images/cabecera.jpg" width="790" height="113" /></div>
+	<div id="informacion"><img src="images/info.jpg" width="790" height="181" /></div>
+	<div id="ranking"><img src="images/fondo.jpg" width="790" height="300" /></div>
+	<div id="pie"><img src="images/pie.jpg" width="790" height="129" /></div> 
+ <div class="fb-comments" data-href="http://growing-dusk-1846.herokuapp.com/rankingII.php" data-num-posts="2" data-width="790"></div>
+ </div>
+</body>
+</html>
